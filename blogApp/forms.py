4fc0fed883dd1c9e6863203ce068353c1145682e
hasdajustin <matheusjustinhasda@gmail.com>
2025-07-23 04:46:41 +0600

    add authnication on login and registration

--- blogApp/forms.py
+++ blogApp/forms.py
@@ -1,8 +1,36 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter password'
+    }))
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter email'
+            }),
+        }
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter username'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter password'
+        })
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import BlogPost
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -7,6 +10,20 @@ def home(request):
     posts = BlogPost.objects.filter(title__icontains=query) if query else BlogPost.objects.all().order_by('-created_at')
     return render(request, 'blog/index.html', {'posts': posts})
 
+@login_required
 def post_detail(request, slug):
     post = get_object_or_404(BlogPost, slug=slug)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
