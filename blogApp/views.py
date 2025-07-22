939a32f384cff52106745793adc84b9c6e9a0c10
hasdajustin <matheusjustinhasda@gmail.com>
2025-07-23 03:02:28 +0600

    add home page and read data from database and and static settings, media root

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def test(request):
-    return render(request, 'test/test.html')
+    query = request.GET.get('q')
+    posts = BlogPost.objects.filter(title__icontains=query) if query else BlogPost.objects.all().order_by('-created_at')
+    return render(request, 'blog/index.html', {'posts': posts})
